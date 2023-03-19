8346d5b8c6fca771a7ab348ad306a8edc10ab741
Andenga <62437418+Andenga@users.noreply.github.com>
2023-03-19 21:00:53 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 
 /**
  * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(void)t
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle);
 	return (0);
 }
