2ccee889b19e3d654a4adf5e808dcdc334e3efc4
Andenga <62437418+Andenga@users.noreply.github.com>
2023-03-23 11:48:16 +0300

    Print numbers from 0 to 9 except a and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,20 @@
 
 /**
  * Print the number 0 to 9 followed by a new line
+ * Don't print 2 and 4.
  *
- * Can only use _putchar twice
+ * Can only use putchar twice
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {   
     int i;
     for (i = 0; i <= 9; i++)
+    if (i != 2 && i != 4){
     {
         _putchar(i + '0');
     }
         _putchar('\n');
-}
+}}
 
 
 
