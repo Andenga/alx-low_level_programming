a6b4adbffff7591de9281eb4deacd84f61e7a298
Andenga <62437418+Andenga@users.noreply.github.com>
2023-03-23 14:45:31 +0300

    Correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - Checks for uppercase characters.
- * @c: The character to be checked.
+ * The character to be checked.
  *
  * Return: 1 if character is uppercase, 0 otherwise.
  */
