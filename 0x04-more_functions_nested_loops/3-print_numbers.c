f77e484815151bd8c7dc7c996a4e6239053d9140
Andenga <62437418+Andenga@users.noreply.github.com>
2023-03-23 11:37:55 +0300

    Print 0 to 9 followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Print the number 0 to 9 followed by a new line
+ *
+ * Multiply inta and intb.
+ */
+void print_numbers(void)
+{   
+    int i;
+    for (i = 0; i <= 9; i++)
+    {
+        _putchar(i + '0');
+    }
+        _putchar('\n');
+}
+
+
+
