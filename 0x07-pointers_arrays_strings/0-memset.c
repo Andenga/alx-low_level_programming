de371918c1efcd57443b4f09c57fc9c5082239f3
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-03 19:56:42 +0300

    Locate a character in a string

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,11 +5,13 @@
  * Fills the memory with a constant byte.
  */
 
-char *_memset(char *s, char b, unsigned int n){
+char *_memset(char *s, char b, unsigned int n)
+{
     
     unsigned int x;
 
-    for(x = 0; x < n; x++){
+    for(x = 0; x < n; x++)
+    {
         s[x] = b;
     }
 
