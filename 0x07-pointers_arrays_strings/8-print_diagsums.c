cb5bc02866d6434a3355aa9a19fd57c843a97f2e
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-03 20:06:20 +0300

    Sum of 2 diagnols

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * Print sum of 2 diagnols.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+
+	unsigned int x, y;
+
+	x = 0;
+	y = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		x = x + a[(size * i) + i];
+		y = y + a[(size * (i + 1)) - (i + 1)];
+	}
+
+	printf("%d, %d\n", y, x);
+}
