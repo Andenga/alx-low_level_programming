1393111b0c79d1ea3fc8c009421d659cb69f68c3
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-05 21:57:01 +0300

    Prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * returns 1 if the input integer is a prime number, otherwise return 0
+*/
+
+
+int actual_prime(int n, int x){
+
+}
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+int actual_prime(int n, int x)
+{
+	if (x == 1)
+		return (1);
+	if (n % x == 0 && x > 0)
+		return (0);
+	return (actual_prime(n, x - 1));
+}
