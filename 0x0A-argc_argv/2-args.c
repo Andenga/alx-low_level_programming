375a0c2b651b76d58d885e7b947cd1f41ef63920
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-09 13:21:26 +0300

    Print all rguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,11 +3,12 @@
 /**
  * Print all arguments it receives.
  */
-int main(int argc, char *argv[])
+int main(int x, char *argv[])
+
 {
 	int arg;
 
-	for (arg = 0; arg < argc; arg++)
+	for (arg = 0; arg < x; arg++)
 		printf("%s\n", argv[arg]);
 
 	return (0);
