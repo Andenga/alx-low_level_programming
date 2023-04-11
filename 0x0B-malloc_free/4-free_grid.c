dd4900939a7fa1f23818c38c3faab08ef6248bfe
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-12 01:08:00 +0300

    Two-dimension grid from previous alloc_grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,10 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 
-/**
- *  free a 2 dimensional grid previously created by your alloc_grid function.
- */
 void free_grid(int **grid, int height)
+
 {
 	if (grid != NULL && height != 0)
 	{
