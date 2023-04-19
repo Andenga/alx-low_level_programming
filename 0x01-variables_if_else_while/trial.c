29e5665894ba24acd06a4248b9c6e163ba10a5ba
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-19 22:27:42 +0300

    Prints the alphabet in reverse

--- 0x01-variables_if_else_while/trial.c
+++ 0x01-variables_if_else_while/trial.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - print all single digit numbers of base 10.
+ * main - print lowercase alphabet in reverse
  * 
- * return: always 0(success).
+ * return: always 0.
 */
 int main(void)
 {
-    int numbers;
+    char letter;
 
-    for(numbers = 0; numbers < 10; numbers++)
-    {
-        char ch = numbers;
-        putchar(ch);
-    }
+    for(letter = 'z'; letter > 'a'; letter--)
+    putchar(letter);
 
     putchar('\n');
-    return(0);
+    
+    return (0);
 }
 
 
@@ -28,14 +26,10 @@ int main(void)
 
 
 
-
-
-
 /**
- * Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+Write a program that prints the lowercase alphabet in reverse, followed by a new line.
 
-You are not allowed to use any variable of type char
 You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
-You can only use putchar twice in your code
 All your code should be in the main function
+You can only use putchar twice in your code
 */
