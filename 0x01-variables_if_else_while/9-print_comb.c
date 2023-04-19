2969812493580c7f4abb8e1c272614355cb3db5a
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-19 23:02:17 +0300

    print all possible combinations of single digigt numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - main block
  * Description: prints all single digit numbers of base 10
  * starting from 0, followed by a new line.
  * Return: 0
  */
-int main(void)
-{
-	int c = 0;
+int main() {
+    int i, j, k;
 
-	while (c < 10)
-	{
-		putchar(48 + c);
-		if (c != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		c++;
-	}
-	putchar('\n');
-	return (0);
+    for(i = 0; i < 10; i++) {
+        for(j = i; j < 10; j++) {
+            for(k = j; k < 10; k++) {
+                putchar(i + '0');
+                putchar(',');
+                putchar(' ');
+                putchar(j + '0');
+                putchar(',');
+                putchar(' ');
+                putchar(k + '0');
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    return 0;
 }
