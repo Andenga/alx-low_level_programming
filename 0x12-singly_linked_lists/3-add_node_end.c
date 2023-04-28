ec77afe7f2c1b6793ac3daa4cf6fba47438a4702
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-29 02:16:01 +0300

    Correct

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,9 +2,9 @@
 
 /**
  * add_node_end - adds a new node at the end
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
+ * @head: head 
+ * 
+ * @str: string 
  * Return: address of the head.
  */
 
