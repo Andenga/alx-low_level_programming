fb4e905add0c0e77ee483db777e0a755c9a84e23
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-29 02:21:22 +0300

    Fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,15 @@
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-
-          global    main
-          extern    printf
+	SECTION .text
+	extern printf
+	global main
 main:
-	  mov   edi, format
-	  call  printf
-	  ret
-format: db `Hello, Holberton\n`,0
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
