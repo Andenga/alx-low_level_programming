6c3e02d4680f5f6f90e3248afa6bf092a5f2da91
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-29 02:20:03 +0300

    Fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
-
 /**
- * first -  prints You're beat! and yet, you must allow,\nI bore my house 
- * upon my back!\n before the main function is executed.
- * 
- * Function runs
- */
-void first(void)
+ * _attribute_  - premain function
+ *
+ * Return: nothing(Success).
+*/
+
+void __attribute__ ((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
