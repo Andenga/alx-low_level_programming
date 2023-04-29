211263ac451e384edc0e23cde03af596baeb125b
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-29 16:14:41 +0300

    Try

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * *reverse_listint - singly linked list function.
+ * @head: integer
+ * @next: points to the next 
+ * @tail: points to the tail.
+ *
+ * Description: singly linked list node structure
+ *
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+
+	return (*head);
+}
