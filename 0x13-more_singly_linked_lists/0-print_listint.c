bb636535305c375e10b9a7205e3c852f8a544012
Andenga <62437418+Andenga@users.noreply.github.com>
2023-04-29 16:29:03 +0300

    FIle correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,26 @@
 #include "lists.h"
 
 /**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/**
  * print_listint - a function that prints all elements of a list
  *
  * @h: pointer to first node
  * Return: the number of nodes
 */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t node_count = 1;
