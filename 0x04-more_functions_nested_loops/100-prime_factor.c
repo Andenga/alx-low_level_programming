40dd56581fe2b6b9212cc65b9e4d3e2dd4175500
Andenga <62437418+Andenga@users.noreply.github.com>
2023-05-02 00:03:55 +0300

    Print largest prime number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long num = 612852475143;
+    long factor = 2;
+
+    while (num > 1)
+    {
+        if (num % factor == 0)
+        {
+            num /= factor;
+            factor--;
+        }
+        factor++;
+    }
+
+    printf("%ld\n", factor);
+
+    return 0;
+}
 
