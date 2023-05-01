e3509bf4ed882e89f84f4418d4f2597fa0ef86aa
Andenga <62437418+Andenga@users.noreply.github.com>
2023-05-01 21:08:01 +0300

    Print from 0 to 9

--- 0x04-more_functions_nested_loops/trial.c
+++ 0x04-more_functions_nested_loops/trial.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
-/**
- * print_numbers(void): Prints numbers from 0 to 9.
- * 
- * 
- * return: always 0(Success).
-*/
 
-print_numbers(void)
-{
-    for(int x = 0; x <= 9; x++)
-    _putchar(x);
-
-    _putchar('/n');
-}
 
 
 
@@ -22,10 +9,8 @@ print_numbers(void)
 /**
  * 
  * 
- * Write a function that prints the numbers, from 0 to 9, followed by a new line.
-
-Prototype: void print_numbers(void);
-You can only use _putchar twice in your code
+ * 
  * 
  * 
 */
+
