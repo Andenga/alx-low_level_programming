339f62c4b2ed523a2326e3fd7584f3c81e0d6260
Andenga <62437418+Andenga@users.noreply.github.com>
2023-05-01 21:08:52 +0300

    Print from 0 to 9

--- 0x04-more_functions_nested_loops/trial.c
+++ 0x04-more_functions_nested_loops/trial.c
@@ -13,4 +13,17 @@
  * 
  * 
 */
+/**
+ * print_numbers(void): Prints numbers from 0 to 9.
+ * 
+ * 
+ * return: always 0(Success).
+*/
 
+print_numbers(void)
+{
+    for(int x = 0; x <= 9; x++)
+    _putchar(x);
+
+    _putchar('/n');
+}
