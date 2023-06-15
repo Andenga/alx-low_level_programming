81a4d2a061642a3d37726e919636a0feec03e535
Andenga <62437418+Andenga@users.noreply.github.com>
2023-06-15 14:08:59 +0300

    Checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocate memory using malloc
+ * @b: the size of the memory to be allocated (in bytes)
+ * 
+ * Return: a pointer to the allocated memory (SUCCESS)
+ * Or normal termination error (FAIL)
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+
+	if (!s)
+		exit(98);
+
+	return (s);
+}
